eea1b362ae6ec33b5bf3c563e3dd8a5afabc49b9
arunbabuceg <150684927+arunbabuceg@users.noreply.github.com>
2023-12-03 23:34:59 +0530

    upto hero

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        custompink: '#FE6B7E'
+      }
+    },
+  },
+  plugins: [],
+}
